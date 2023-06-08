3ec7ce2b079ffc706c7f899a548f7154782ffd17
vadim l <vadimlozinschii@gmail.com>
2023-06-08 12:35:52 -0700

    adds basic tailwind css and component files

--- server/models.py
+++ server/models.py
@@ -3,3 +3,12 @@ from sqlalchemy_serializer import SerializerMixin
 from config import db
 
 # Models go here!
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+
+
+class Recipe(db.Model, SerializerMixin):
+    __tablename__ = 'recipe'
+    id = db.Column(db.Integer, primary_key=True)
