160c3c9d2340d121d978bd8934e382980a99751c
vadim l <vadimlozinschii@gmail.com>
2023-06-08 16:00:18 -0700

    finalizing colors updating tailwindcss

---
+++ client/src/components/Jobs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'tailwindcss/tailwind.css';
+
+function Jobs() {
+    return (
+        <div className="bg-orange-200">
+        <h1 className="text-4xl font-bold text-orange-500">Jobs</h1>
+        {/* Add your job listing components or logic here */}
+        </div>
+    );
+}
+
+export default Jobs;
