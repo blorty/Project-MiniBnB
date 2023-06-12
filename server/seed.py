f5e1eda457eebafdd887bc12a8dc6168d1fddfa4
+ <familymanjosh@gmail.com>
2023-06-11 20:09:11 -0700

    Seeding Complete

--- server/seed.py
+++ server/seed.py
@@ -2,8 +2,10 @@ from flask import Flask
 from flask_migrate import Migrate
 from models import db, User, Job, AppliedJob, Salary, CompanyReview, Company
 from config import app
-from random import randint, choice as rc
+from random import randint
 from faker import Faker
+from flask_bcrypt import Bcrypt
+from datetime import date
 
 migrate = Migrate(app, db)
 
@@ -14,81 +16,71 @@ with app.app_context():
     print("Creating tables...")
     db.create_all()
 
+    fake = Faker()
 
+    for _ in range(5, 21):
+        user = User()
+        user.username = fake.user_name()
+        user.email = fake.email()
+        user._password_hash = fake.password()
+        db.session.add(user)
 
-# Seed data
-fake = Faker()
+    db.session.commit()
 
-for _ in range(100):
-    user = User(
-        name=fake.name(),
-        email=fake.email(),
-        password=fake.password()
-    )
-    db.session.add(user)
+    for _ in range(100):
+        job = Job(
+            title=fake.job(),
+            description=fake.text(),
+            location=fake.city(),
+            salary=fake.random_int(),
+            User_id=randint(1, 100)
+        )
+        db.session.add(job)
 
-db.session.commit()
+    db.session.commit()
 
-for _ in range(100):
-    job = Job(
-        title=fake.job(),
-        description=fake.text(),
-        location=fake.city(),
-        salary=fake.random_int(),
-        user_id=randint(1, 100)
-    )
-    db.session.add(job)
+    for _ in range(100):
+        applied_job = AppliedJob(
+            user_id=randint(1, 100),
+            job_id=randint(1, 100),
+            salary=fake.random_int(min=50000, max=100000),
+            company_review=fake.text(),
+            applied_date=date.today()
+        )
+        db.session.add(applied_job)
 
-db.session.commit()
+    db.session.commit()
 
-for _ in range(100):
-    saved_job = SavedJob(
-        user_id=randint(1, 100),
-        job_id=randint(1, 100)
-    )
-    db.session.add(saved_job)
+    for _ in range(100):
+        salary = Salary(
+            salary=fake.random_int(min=50000, max=100000),
+            job_id=randint(1, 100),
+            User_id=randint(1, 100)
+        )
+        db.session.add(salary)
 
-db.session.commit()
+    db.session.commit()
 
-for _ in range(100):
-    applied_job = AppliedJob(
-        user_id=randint(1, 100),
-        job_id=randint(1, 100)
-    )
-    db.session.add(applied_job)
+    for _ in range(100):
+        company_review = CompanyReview(
+            review=fake.text(),
+            User_id=randint(1, 100),
+            company_id=randint(1, 100)
+        )
+        db.session.add(company_review)
 
-db.session.commit()
+    db.session.commit()
 
-for _ in range(100):
-    salary = Salary(
-        salary=fake.random_int(),
-        user_id=randint(1, 100)
-    )
-    db.session.add(salary)
+    for _ in range(100):
+        company = Company(
+            name=fake.company(),
+            description=fake.text(),
+            location=fake.city(),
+            industry=fake.job(),
+            website=fake.url()
+        )
+        db.session.add(company)
 
-db.session.commit()
-
-for _ in range(100):
-    company_review = CompanyReview(
-        review=fake.text(),
-        user_id=randint(1, 100),
-        company_id=randint(1, 100)
-    )
-    db.session.add(company_review)
-
-db.session.commit()
-
-for _ in range(100):
-    company = Company(
-        name=fake.company(),
-        description=fake.text(),
-        location=fake.city(),
-        industry=fake.job(),
-        website=fake.url()
-    )
-    db.session.add(company)
-
-db.session.commit()
+    db.session.commit()
 
 print("Seeding complete!")
-
