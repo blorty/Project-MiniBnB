2c3db0ddbf5322baa95975877cd0eb973ad11086
+ <familymanjosh@gmail.com>
2023-06-13 08:59:12 -0700

    fully commiting

--- client/src/app.js
+++ client/src/app.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch} from "react-router-dom";
 import NavBar from "./components/NavBar";
-import Home from "./components/Home";
+
+import './scrollbar.css'
+
+import "./style.css";
 
 function App() {
+
     return (
         <div >
         <Router>
+        <Router>
             <NavBar />
             <Switch>
-            <Route exact path="/" component={Home} />
-            {/* Add more routes here */}
+
             </Switch>
+        </Router>
         </Router>
         </div>
     );
