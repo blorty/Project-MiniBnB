9a2ee92dde27c1953cac98e7d43025d370cf5fbd
+ <familymanjosh@gmail.com>
2023-06-13 10:47:10 -0700

    Adding APP

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+
+function App() {
+    return (
+        <div >
+        <Router>
+            <NavBar />
+            <Switch>
+            <Route exact path="/" component={Home} />
+            {/* Add more routes here */}
+            </Switch>
+        </Router>
+        </div>
+    );
+}
+
+export default App;
